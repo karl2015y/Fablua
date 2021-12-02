fc63c07b05a21dfcbc47ce401b761d31b2c033b8
karl2015y <karl2015y@gmail.com>
2021-12-03 07:01:57 +0800

    第一頁排版完成

--- Home/js/home.js
+++ Home/js/home.js
@@ -1,9 +1,70 @@
 var app = new Vue({
-    el: '#app',
-    components: {
-        agile: VueAgile
+  el: '#app',
+  components: {
+    agile: VueAgile
+  },
+  data: function () {
+    return {
+
+      menu_open: false,
+      scroll_top_offset: null,
+    };
+  },
+
+  created: function () {
+    const vm = this;
+    window.onscroll = vm.onscroll;
+    vm.scrollParallax()
+  },
+  methods: {
+    onscroll: function () {
+      const vm = this;
+      vm.scroll_top_offset = window.pageYOffset;
     },
-    data: {
-      menu_open: false
+    elementInViewport: function (el) {
+      var top = el.offsetTop;
+      var left = el.offsetLeft;
+      var width = el.offsetWidth;
+      var height = el.offsetHeight;
+
+      while (el.offsetParent) {
+        el = el.offsetParent;
+        top += el.offsetTop;
+        left += el.offsetLeft;
+      }
+
+      return (
+        top < (window.pageYOffset + window.innerHeight) &&
+        left < (window.pageXOffset + window.innerWidth) &&
+        (top + height) > window.pageYOffset &&
+        (left + width) > window.pageXOffset
+      );
+    },
+    scrollParallax: function (css_selector = 'img.offset-img') {
+      const vm = this;
+      const handler = (vm, css_selector ) => {
+        document.querySelectorAll(css_selector).forEach((el) => {
+          if (vm.elementInViewport(el.parentElement)) {
+            el.style.top = `-${el.parentElement.getBoundingClientRect().top}px`
+          } else {
+            el.style.top = `0px`
+          }
+        })
+      };
+      handler(vm,css_selector);
+      let time_out = null;
+      window.addEventListener('scroll', () => {
+        handler(vm,css_selector)
+        // if (time_out) {
+        //   clearTimeout(time_out);
+        // }
+        // time_out = setTimeout(()=>{handler(vm,css_selector)}, 1);
+
+      });
     }
-  })
+
+  },
+  watch: {
+
+  }
+})
--- Home/js/index.js
+++ Home/js/index.js
@@ -7,19 +7,15 @@ function reportWindowSize() {
     let fs = 16;
     console.log(currentSize, PhoneSize);
     if(currentSize<=PhoneSize){
-        console.log(1)
         fs = 3.4;
     }else if(currentSize>DesktopSize){
-        console.log(1)
         fs = 1.1;
     }else{
-        console.log(3)
         fs = (-0.00357*currentSize)+6.14
         fs = fs >2?2:fs;
     }
 
     document.querySelector('html').style.fontSize=`${fs}vw`
-    console.log(document.querySelector('html').style.fontSize )
 }
 
 window.onresize = reportWindowSize;
@@ -27,3 +23,5 @@ window.addEventListener("load", function(event) {
     reportWindowSize()
     
   });
+
+
